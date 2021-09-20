33f7612c7a8ea79d866f32e854c18b5230f8c8cc
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-09-19 22:17:26 -0700

    task: create contact controller

---
+++ src/main/java/com/cristinasatterfield/technicalassessment/contact/ContactController.java
@@ -0,0 +1,80 @@
+package com.cristinasatterfield.technicalassessment.contact;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/contact")
+public class ContactController {
+  @Autowired
+  private ContactRepository contactRepository;
+
+  // GET method to fetch all contacts or contacts that match the optional name parameter
+  @GetMapping
+  public List<Contact> getAllContacts(@RequestParam(required = false) String name) {
+    if (name == null) {
+      return contactRepository.findAll();
+    }
+    return contactRepository.findByNameContainingIgnoreCase(name);
+  }
+
+  // GET method to fetch contact by contactId
+  @GetMapping("/{contactId}")
+  public ResponseEntity<Contact> geContactById(@PathVariable Long contactId) {
+    final Optional<Contact> contact = contactRepository.findById(contactId);
+
+    if (!contact.isPresent()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(contact.get(), HttpStatus.OK);
+  }
+
+
+  // POST method to create a contact
+  @PostMapping
+  public Contact createContact(@Valid @RequestBody Contact contact) {
+      return contactRepository.save(contact);
+  }
+
+  /**
+   * PUT method to update a contacts's details
+   *
+   * @param contactId The id of the contact to update
+   * @param contactDetails The contact fields to update
+   * @returns The updated contact or 404 NOT FOUND
+   */
+  @PutMapping("/{contactId}")
+  public ResponseEntity<Contact> updateContact(@PathVariable Long contactId, @Valid @RequestBody Contact contactDetails) {
+    final Optional<Contact> contactQuery = contactRepository.findById(contactId);
+    if (!contactQuery.isPresent()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    final Contact contact = contactQuery.get();
+    contact.setName(contactDetails.getName());
+    return new ResponseEntity<>(contactRepository.save(contact), HttpStatus.OK);
+  }
+
+  // DELETE method to delete a contact
+  @DeleteMapping("/{contactId}")
+  public void deleteContact(@PathVariable Long contactId) {
+    contactRepository.deleteById(contactId);
+  }
+
+}
--- src/main/java/com/cristinasatterfield/technicalassessment/contact/ContactRepository.java
+++ src/main/java/com/cristinasatterfield/technicalassessment/contact/ContactRepository.java
@@ -1,12 +1,11 @@
 package com.cristinasatterfield.technicalassessment.contact;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*
- * The interface Phone repository.
- */
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-
+  List<Contact> findByNameContainingIgnoreCase(String name);
 }
