1d7edc9d562452b50ab03918fded1be47eead823
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-09-23 23:22:05 -0700

    task: add API documentation using OpenAPI and ReDoc

--- src/main/java/com/cristinasatterfield/technicalassessment/contact/ContactController.java
+++ src/main/java/com/cristinasatterfield/technicalassessment/contact/ContactController.java
@@ -21,8 +21,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/contact")
+@Tag(name = "Contact API", description = "CRUD API endpoints for managing contacts.")
 public class ContactController {
   @Autowired
   private ContactService contactService;
@@ -34,6 +42,10 @@ public class ContactController {
    * @returns A array of contacts that match the parameter or an empty array
    */
   @GetMapping
+  @Operation(
+    summary = "Fetch contacts",
+    description = "Fetch all contacts if no name is provided. If a name is provided, fetch all contacts that match."
+  )
   public List<Contact> getAllContacts(@RequestParam(required = false) String name) {
     return this.contactService.getAllContacts(name);
   }
@@ -45,7 +57,14 @@ public class ContactController {
    * @returns The requested contact or 404 NOT FOUND
    */
   @GetMapping("/{contactId}")
-  public ResponseEntity<Contact> geContactById(@PathVariable Long contactId) {
+  @Operation(summary = "Fetch a contact by Id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Successfully returned the contact",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Contact.class)) }),
+    @ApiResponse(responseCode = "404", description = "Contact not found",
+      content = @Content) })
+  public ResponseEntity<Contact> getContactById(@PathVariable Long contactId) {
     final Optional<Contact> contact = this.contactService.getContactById(contactId);
 
     if (!contact.isPresent()) {
@@ -61,6 +80,13 @@ public class ContactController {
    * @returns The created contact, 400 if bad request, or 500 Internal Service Error
    */
   @PostMapping
+  @Operation(summary = "Create a new contact")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Successfully created and returned the contact",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Contact.class)) }),
+    @ApiResponse(responseCode = "400", description = "Invalid request; contact not created ",
+      content = @Content) })
   public Contact createContact(@Valid @RequestBody CreateContactDto contact) {
       return this.contactService.createContact(contact);
   }
@@ -73,6 +99,15 @@ public class ContactController {
    * @returns The updated contact or 404 NOT FOUND
    */
   @PutMapping("/{contactId}")
+  @Operation(summary = "Update a contact by id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Successfully updated and returned the contact",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Contact.class)) }),
+    @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+      content = @Content),
+    @ApiResponse(responseCode = "404", description = "Contact not found",
+      content = @Content) })
   public ResponseEntity<Contact> updateContact(@PathVariable Long contactId, @Valid @RequestBody UpdateContactDto contactDto) {
     final Optional<Contact> contactQuery = this.contactService.getContactById (contactId);
     if (!contactQuery.isPresent()) {
@@ -89,6 +124,15 @@ public class ContactController {
    * @param contactId The id of the contact to delete
    */
   @DeleteMapping("/{contactId}")
+  @Operation(summary = "Delete a contact by id")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "204", description = "Successfully deleted the contact",
+      content = { @Content(mediaType = "application/json",
+        schema = @Schema(implementation = Void.class)) }),
+    @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+      content = @Content),
+    @ApiResponse(responseCode = "404", description = "Contact not found",
+      content = @Content) })
   public ResponseEntity<Void> deleteContact(@PathVariable Long contactId) {
     final Optional<Contact> contactQuery = this.contactService.getContactById (contactId);
     if (!contactQuery.isPresent()) {
